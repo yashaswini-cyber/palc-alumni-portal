845986ee39cb94e7bd9a86af4012d4c640d5a9c5
Yashaswini Pawar <pawar.rvu@gmail.com>
2026-08-03 22:41:44 +0530

    Backend is running on the local host now.

--- backend/server.ts
+++ backend/server.ts
@@ -12,7 +12,20 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("PalC Alumni Backend Running");
 });
+app.post("/api/send-otp", async (req, res) => {
 
+    const { email, otp } = req.body;
+
+    console.log("========== OTP REQUEST ==========");
+    console.log("Email :", email);
+    console.log("OTP   :", otp);
+    console.log("=================================");
+
+    res.json({
+        success: true
+    });
+
+});
 const PORT = 5000;
 
 app.listen(PORT, () => {
