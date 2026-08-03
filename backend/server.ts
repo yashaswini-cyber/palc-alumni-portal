06fddfe2ad604da2f155e713bc453f6ddb5a3c9c
Yashaswini Pawar <pawar.rvu@gmail.com>
2026-08-03 23:03:51 +0530

    Added try and catch blocks to the server.ts; Next step is fixing emailServices.

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { sendOTPEmail } from "./emailService.js";
 
 dotenv.config();
 
@@ -13,17 +14,24 @@ app.get("/", (req, res) => {
     res.send("PalC Alumni Backend Running");
 });
 app.post("/api/send-otp", async (req, res) => {
-
+    try{
     const { email, otp } = req.body;
 
-    console.log("========== OTP REQUEST ==========");
+    console.log("OTP REQUEST");
     console.log("Email :", email);
     console.log("OTP   :", otp);
     console.log("=================================");
-
+    await sendOTPEmail(email, otp);
     res.json({
         success: true
     });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            success: false
+        });
+    }
 
 });
 const PORT = 5000;
